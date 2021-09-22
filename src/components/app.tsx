39d25e48fdb41d2f2e65c8e7bcddb58aa064c43b
Max16 <max16@naver.com>
2021-09-22 20:02:51 +0900

    frontend test setting

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,16 +1,14 @@
 import { useReactiveVar } from "@apollo/client";
-import { LoggedInRouter } from "./routers/logged-in-router";
-import { LoggedOutRouter } from "./routers/logged-out-router";
-import { isLoggedInVar } from "./apollo";
+import { LoggedInRouter } from "../routers/logged-in-router";
+import { LoggedOutRouter } from "../routers/logged-out-router";
+import { isLoggedInVar } from "../apollo";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
 
 library.add(fas, far);
 
-function App() {
+export const App = () => {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   return isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />;
-}
-
-export default App;
+};
